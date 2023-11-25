7510840b4ed9f5ec6f672e9ba77f03e9b6ac3509
mike <namike623@gmail.com>
2023-11-25 02:26:27 +0000

    recover moviedb-promise

--- src/components/carosel.svelte
+++ src/components/carosel.svelte
@@ -1,57 +1,39 @@
 <script lang="ts">
+  import Carousel from "svelte-carousel";
   import API, { type MovieResult, type MovieResponse } from "../api";
   import { createEventDispatcher } from "svelte";
   const dispatch = createEventDispatcher();
   export let movies: MovieResult[] = [];
   export let selectedMovie: MovieResponse | undefined;
+
   function selectMoive(movie, e) {
     dispatch("selectMoive", { movie, e });
-  }
-  function isSelected(movie: MovieResult) {
-    return selectedMovie?.id === movie.id;
   }
 </script>
 
 {#if movies.length > 0}
-  <div
-    class="flex -mx-5 px-5 py-10 post-list scrollbar scrollbar-thumb-gray-100 scrollbar-track-transparent scrollbar-thin"
-  >
+  <!-- class="flex -mx-5 px-5 py-10 post-list scrollbar scrollbar-thumb-gray-100 scrollbar-track-transparent scrollbar-thin" -->
+  <Carousel particlesToShow={5}>
     {#each movies as movie, i}
-      <div
-        tabindex={i}
-        role="button"
-        on:click={(e) => selectMoive(movie, e)}
-        class="poster-preview opacity-30 cursor-pointer"
-        class:opacity-100={isSelected(movie)}
-        style="background-image: url({API.getImagePath(
-          movie.poster_path || movie.backdrop_path,
-          { w: 185 }
-        )});"
-      />
+      <div class="h-[400px] grid relative">
+        <div class="absolute inset-5 bg-slate-800 overflow-hidden">
+          <button
+            tabindex={i}
+            role="checkbox"
+            on:click={(e) => selectMoive(movie, e)}
+            class:opacity-100={movie.id === selectedMovie?.id}
+            class:opacity-30={movie.id !== selectedMovie?.id}
+            class=" cursor-pointer w-full h-full bg-no-repeat bg-center bg-cover transition-opacity aria-checked:opacity-100"
+            style="background-image: url({API.getImagePath(
+              movie.poster_path || movie.backdrop_path,
+              { w: 185 }
+            )});"
+          />
+        </div>
+      </div>
     {/each}
-  </div>
+  </Carousel>
 {/if}
 
 <style>
-  .poster-preview {
-    flex-shrink: 0;
-    margin: 0 16px;
-    width: 150px;
-    height: 225px;
-    background-size: cover;
-    background-position: center;
-    border-radius: 4px;
-    /* opacity: 0.5; */
-  }
-  .poster-preview.selected {
-    opacity: 1;
-  }
-  .poster-preview:first-child {
-    margin-left: 0;
-  }
-  .poster-preview:hover {
-    opacity: 1;
-    transform: scale(1.1);
-    transition: all 0.2s ease;
-  }
 </style>
